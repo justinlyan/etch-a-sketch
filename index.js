01727fad8c4fdadefbc3369f9a334404faa143ba
Justin Lyan <33265378+justinlyan@users.noreply.github.com>
2024-11-09 19:24:18 -0600

    squares change background whne hovered, users can choose how many squares per row

--- index.js
+++ index.js
@@ -1,12 +1,39 @@
-let container = document.querySelector(".container");
+const container = document.querySelector(".container");
+const btn = document.querySelector("button");
 
-for (let i = 0; i < 16; i++) {
-  let row = document.createElement("div");
-  row.classList.add("row");
-  for (let i = 0; i < 16; i++) {
-    let square = document.createElement("div");
-    square.classList.add("square");
-    row.appendChild(square);
+btn.addEventListener("click", () => {
+  let numSquares = requestSquares();
+  while (numSquares > 100) {
+    numSquares = requestSquares();
   }
-  container.appendChild(row);
-};
+
+  container.innerHTML = '';
+  makeGrid(numSquares);
+});
+
+const requestSquares = () => {
+  return prompt("How many squares per side?");
+}
+
+const makeGrid = (numSquaresPerRow) => {
+  for (let i = 0; i < numSquaresPerRow; i++) {
+    const row = document.createElement("div");
+    row.classList.add("row");
+    for (let i = 0; i < numSquaresPerRow; i++) {
+      let sideLength = 960 / numSquaresPerRow;
+      const square = document.createElement("div");
+      square.classList.add("square");
+      square.classList.add("square");
+      square.style.height = sideLength + "px";
+      square.style.width = sideLength + "px";
+      row.appendChild(square);
+  
+      square.addEventListener("mouseover", (event) => {
+        event.target.classList.add("hovered");
+      })
+    }
+    container.appendChild(row);
+  };
+}
+
+makeGrid(16);
